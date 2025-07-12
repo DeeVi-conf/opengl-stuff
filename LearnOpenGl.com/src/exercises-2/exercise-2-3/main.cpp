9b5a1da164193fece10db842e3dd644ea23a5e3f
Jaiveer2525 <jayeshbisht25@gmail.com>
2025-07-12 13:23:16 +0530

    stuff

---
+++ LearnOpenGl.com/src/exercises-2/exercise-2-3/main.cpp
@@ -0,0 +1,103 @@
+/** Output the vertex position to the fragment shader using the out keyword and set the fragment's color equal to this vertex position (see how even the vertex position values are interpolated across the triangle). Once you managed to do this; try to answer the following question: why is the bottom-left side of our triangle black? */
+
+#include <glad/glad.h>
+#include <shader/shader_s.hpp>
+#include <GLFW/glfw3.h>
+
+const uint SCR_WIDTH = 800;
+const uint SCR_HEIGHT = 600;
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    //data
+    float vertices[] = {
+        // positions                       // colors
+          .5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    }; 
+
+
+    Shader shaderProgram("src/exercises-2/exercise-2-3/shader.vs","src/exercises-2/exercise-2-3/shader.fs");
+
+
+    // Main Initialization
+    unsigned int vertex_buffer_object;
+    glGenBuffers(1, &vertex_buffer_object);
+    unsigned int VertexArrayObject;
+    glGenVertexArrays(1, &VertexArrayObject); 
+
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VertexArrayObject);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object); 
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0); 
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1); 
+
+    // wireframe 
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // main rendering loop
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //glUseProgram(shaderProgram);
+        shaderProgram.use();
+
+        glBindVertexArray(VertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteBuffers(1, &vertex_buffer_object);
+    glDeleteVertexArrays(1, &VertexArrayObject);
+    //glDeleteProgram(shaderProgram);
+    glfwDestroyWindow(window);
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
---
+++ LearnOpenGl.com/src/exercises-2/exercise-2-3/shader.fs
@@ -0,0 +1,8 @@
+#version 330 core
+
+out vec4 FragColor;
+in vec3 PosColor;
+
+void main(){
+    FragColor = vec4(PosColor, 1.0f);
+}
